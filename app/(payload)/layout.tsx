073fe957f9789d9165aa5d12ac1c2b796c3abeb3
Flayrox <epheofc@gmail.com>
2026-04-15 01:12:26 +0200

    Fix Payload admin hydration layout

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -1,31 +1,14 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
-import config from '@payload-config';
 import '@payloadcms/next/css';
-import { RootLayout, handleServerFunctions } from '@payloadcms/next/layouts';
 import React from 'react';
 
-import { importMap } from './admin/importMap.js';
 import './custom.scss';
 
 type Args = {
     children: React.ReactNode;
 };
 
-const serverFunction = async (
-    ...args: Parameters<typeof handleServerFunctions>
-) => {
-    'use server';
-
-    return handleServerFunctions(...args);
-};
-
-const Layout = ({ children }: Args) =>
-    RootLayout({
-        children,
-        config,
-        importMap,
-        serverFunction,
-    });
+const Layout = ({ children }: Args) => <>{children}</>;
 
 export default Layout;
